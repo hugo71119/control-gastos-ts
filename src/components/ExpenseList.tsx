bc1f1a4088ac773b129df2b32e0e39136ed80857
hugo71119 <hugo71119@gmail.com>
2024-09-26 12:15:45 -0600

    finalizando el proyecto

--- src/components/ExpenseList.tsx
+++ src/components/ExpenseList.tsx
@@ -5,13 +5,14 @@ import ExpenseDetail from "./ExpenseDetail"
 export default function ExpenseList() {
     const { state } = useBudget()
 
-    const isEmpty = useMemo(() => state.expenses.length === 0, [state.expenses])
+    const filterExpense = state.currentCategory ? state.expenses.filter( expense => expense.category === state.currentCategory) : state.expenses
+    const isEmpty = useMemo(() => filterExpense.length === 0, [state.expenses])
   return (
     <div className=" mt-10">
         {isEmpty ? <p className="text-gray-600 text-2xl font-bold">No hay gastos</p> : (
             <>
                 <p className="text-gray-600 text-2xl font-bold my-5">Listado de gastos</p>
-                {state.expenses.map(expense => (
+                {filterExpense.map(expense => (
                     <ExpenseDetail
                         key={expense.id}
                         expense={expense}
